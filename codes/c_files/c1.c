28e371a71612c9c3e889fd1541e511fca5cd3815
SungJae-An <anthony06090612@gmail.com>
2025-08-16 16:10:15 +0900

    0816 notes, several c/c++ pratices.

---
+++ codes/c_files/c1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("C language test on local environment!\n");
+    return 0;
+}
---
+++ codes/c_files/c3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    int arr[5] = {0,1,2,3,4};
+    printf("arr[0] = %d\n",arr[0]);
+    printf("arr[1] = %d\n",arr[1]);
+    printf("arr[2] = %d\n",arr[2]);
+    printf("arr[3] = %d\n",arr[3]);
+    printf("arr[4] = %d\n",arr[4]);
+    return 0;
+}
